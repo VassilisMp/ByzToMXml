449d2dfbf15f81975c97581b9ca1579eba3d04db
VassilisMp <bill-br@hotmail.com>
2020-12-20 15:34:48 +0200

    fixed engine tests with new folders

--- src/test/java/parser/EngineTest.kt
+++ src/test/java/parser/EngineTest.kt
@@ -1,48 +1,46 @@
 package parser
 
 import org.antlr.v4.runtime.InputMismatchException
-import org.junit.jupiter.api.Test
+import org.apache.commons.io.FilenameUtils
+import org.audiveris.proxymusic.ScorePartwise
+import org.audiveris.proxymusic.util.Marshalling
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+import java.io.File
+import java.io.FileOutputStream
+import javax.xml.bind.Marshaller.JAXB_FORMATTED_OUTPUT
 
 // TODO fill assertions
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 internal class EngineTest {
 
-    @Test
-    fun makarios_anirTest() {
-        val engine = Engine("makarios_anir_syntoma_fokaeos_simple.docx")
+    @BeforeAll
+    fun init() {
+        File(savePath).mkdir()
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["dynamis_k_ioannidh_simple.docx", "doxologia_hxos_a_petrou_argh.docx"
+        , "makarios_anir_syntoma_fokaeos_simple.docx", "oimoi.docx"])
+    fun testSamples(fileName: String) {
+        val engine = Engine("$filePath${File.separator}$fileName")
         try {
-            engine.run()
+            val scorePartwise = engine.run()
+            FileOutputStream("$savePath${File.separator}${fileName.removeExtension()}.xml").use {
+                val marshaller = Marshalling.getContext(ScorePartwise::class.java).createMarshaller();
+                marshaller.setProperty(JAXB_FORMATTED_OUTPUT, true);
+                marshaller.marshal(scorePartwise, it)
+            }
         } catch (e: InputMismatchException) {
             println("caught")
         }
     }
 
-    @Test
-    fun doxologia_hxos_a_petrou_arghTest() {
-        val engine = Engine("doxologia_hxos_a_petrou_argh.docx")
-        try {
-            engine.run()
-        } catch (e: InputMismatchException) {
-            println("caught")
-        }
-    }
-
-    @Test
-    fun dynamis_k_ioannidh_simpleTest() {
-        val engine = Engine("dynamis_k_ioannidh_simple.docx")
-        try {
-            engine.run()
-        } catch (e: InputMismatchException) {
-            println("caught")
-        }
-    }
-
-    @Test
-    fun nekrTest() {
-        val engine = Engine("oimoi.docx")
-        try {
-            engine.run()
-        } catch (e: InputMismatchException) {
-            println("caught")
-        }
+    companion object {
+        const val savePath = "sample_tests_output"
+        const val filePath = "sample_docs"
+        private fun String.removeExtension() = FilenameUtils.removeExtension(this)
     }
 }
